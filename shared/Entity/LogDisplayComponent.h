d1b705a0cf0a583363dc5215769296e46403ec58
mrfun <mrfun@353e56fe-9613-0410-8469-b96ad8e6f29c>
2011-03-18 10:12:37 +0000

    * LogDisplayComponent improved, if you call "AddLine" on it to start adding lines, it will display its own custom log instead of the system log * Started moving some RTsoft private commercial code to the svn server.. these are not public so you shouldn't be able to access RTBattle, RTDink, RTMindWall etc.  If you can, please let me know fast.. ^_^;;

--- shared/Entity/LogDisplayComponent.h
+++ shared/Entity/LogDisplayComponent.h
@@ -12,7 +12,22 @@
 
 #include "Entity/Component.h"
 #include "Entity/Entity.h"
-//#include "Renderer/SurfaceAnim.h"
+#include "Manager/Console.h"
+
+/*
+This shows a log of stuff, scrolling so the newest stuff is on the bottom.
+If you use GetFunction("AddLine") (send it a VariantList with a string) to add your own stuff, it will use its internal logging
+system.  
+
+Otherwise, it will display the proton system log.
+
+An easy way to turn use this to show the system log is to use the helper function below.
+
+Just do:
+
+SetConsole(true); and it will create it as an overlay.
+
+*/
 
 class LogDisplayComponent: public EntityComponent
 {
@@ -23,11 +38,13 @@ public:
 	virtual void OnAdd(Entity *pEnt);
 	virtual void OnRemove();
 
+	//if you REALLY need direct access to this...
+	Console * GetConsole() {return m_pActiveConsole;}
+
 private:
 
 	void OnRender(VariantList *pVList);
-	void OnUpdate(VariantList *pVList);
-
+	void AddLine(VariantList *pVList);
 	CL_Vec2f *m_pPos2d;
 	CL_Vec2f *m_pSize2d;
 	
@@ -39,8 +56,10 @@ private:
 	uint32 *m_pAlignment;
 	float *m_pRotation; //in degrees
 	*/
-
+	Console * m_pActiveConsole;
+	Console * m_pInternalConsole;
 };
+
 void ToggleConsole();
 void SetConsole(bool bOn);
 #endif // LogDisplayComponent_h__
