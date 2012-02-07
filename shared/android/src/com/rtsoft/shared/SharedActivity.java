696de1cef5ee234265c07afe4879462af5d56ab4
aki <aki@353e56fe-9613-0410-8469-b96ad8e6f29c>
2012-02-07 17:40:45 +0000

    * Changed usage of VIRTUAL_DPAD_BUTTON_B constant to VIRTUAL_DPAD_BUTTON_DOWN since the constant was renamed. Fixes the compilation.

--- shared/android/src/com/rtsoft/shared/SharedActivity.java
+++ shared/android/src/com/rtsoft/shared/SharedActivity.java
@@ -572,7 +572,7 @@ return m_szDevIDShort;
 		{
 			//XPeria's O button, not the back button!
 			//Log.v("onKeyDown","Sending xperia back key");
-			nativeOnKey(1, VIRTUAL_DPAD_BUTTON_B, e.getUnicodeChar()); 
+			nativeOnKey(1, VIRTUAL_DPAD_BUTTON_DOWN, e.getUnicodeChar()); 
 		
 			return true; //signal that we handled it
 		}
@@ -600,7 +600,7 @@ return m_szDevIDShort;
 		{
 			//XPeria's O button, not the back button!
 			//Log.v("onKeyDown","Sending xperia back key");
-			nativeOnKey(0, VIRTUAL_DPAD_BUTTON_B, e.getUnicodeChar()); 
+			nativeOnKey(0, VIRTUAL_DPAD_BUTTON_DOWN, e.getUnicodeChar()); 
 		
 			return true; //signal that we handled it
 		}
