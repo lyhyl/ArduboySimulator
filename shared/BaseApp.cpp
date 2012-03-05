06a06e349271905026978226ab6809c706f6f8c8
aki <aki@353e56fe-9613-0410-8469-b96ad8e6f29c>
2012-03-05 08:39:43 +0000

    * BUGFIX: set the variant before reading it

--- shared/BaseApp.cpp
+++ shared/BaseApp.cpp
@@ -254,14 +254,14 @@ void BaseApp::OnMessage(Message &m)
 						break;
 					}
 
+					v.Get(0).Set(float(m.GetType()));
+					v.Get(1).Set(float(m.GetParm1()), float(m.GetParm2()) );
+					v.Get(2).Set(uint32(m.GetParm3()));
+
 					if (m.GetType() == MESSAGE_TYPE_GUI_CLICK_MOVE)
 					{
 						m_touchTracker[m.GetParm3()].SetPos(v.Get(1).GetVector2());
 					}
-
-					v.Get(0).Set(float(m.GetType()));
-					v.Get(1).Set(float(m.GetParm1()), float(m.GetParm2()) );
-					v.Get(2).Set(uint32(m.GetParm3()));
 
 					if (m_inputMode == INPUT_MODE_NORMAL)
 					{
