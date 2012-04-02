9a655c788e24ff4fc37c8c74ffd2d17cc4b077e8
aki <aki@353e56fe-9613-0410-8469-b96ad8e6f29c>
2012-04-02 17:48:00 +0000

    * Removed excess stuff from the sprite sheet example application

--- RTSpriteSheets/source/App.cpp
+++ RTSpriteSheets/source/App.cpp
@@ -2,15 +2,10 @@
  *  App.cpp
  *  Created by Aki Koskinen on 2/4/12.
  *  For license info, check the license.txt file that should have come with this.
- *
  */
 
-//#include "PlatformPrecomp.h"
 #include "App.h"
 #include "GUI/MainMenu.h"
-//#include "Renderer/LinearParticle.h"
-//#include "Entity/EntityUtils.h"
-//#include "Renderer/SoftSurface.h"
 
 MessageManager g_messageManager;
 MessageManager * GetMessageManager() {return &g_messageManager;}
@@ -55,7 +50,6 @@ void App::OnExitApp(VariantList *pVarList)
 
 bool App::Init()
 {
-//	SetDefaultButtonStyle(Button2DComponent::BUTTON_STYLE_CLICK_ON_TOUCH_RELEASE);
 	SetManualRotationMode(true);
 
     bool bScaleScreenActive = true; //if true, we'll stretch every screen to the coords below
@@ -109,9 +103,6 @@ else
 	
 	if (!BaseApp::Init()) return false;
 
-
-//	LogMsg("Save path is %s", GetSavePath().c_str());
-
 	if (!GetFont(FONT_SMALL)->Load("interface/font_trajan.rtfont")) 
 	{
 		LogMsg("Can't load font 1");
@@ -122,28 +113,17 @@ else
 		LogMsg("Can't load font 2");
 		return false;
 	}
-	//GetFont(FONT_SMALL)->SetSmoothing(false); //if we wanted to disable bilinear filtering on the font
 
 	GetBaseApp()->SetFPSVisible(true);
 	
-//	bool bFileExisted;
-//	m_varDB.Load("save.dat", &bFileExisted);
- 
 	//preload audio
 	GetAudioManager()->Preload("audio/click.wav");
 
 	return true;
 }
 
-//void App::SaveOurStuff()
-//{
-//	LogMsg("Saving our stuff");
-//	m_varDB.Save("save.dat");
-//}
-
 void App::Kill()
 {
-//	SaveOurStuff();
 	BaseApp::Kill();
 	g_pApp = NULL;
 }
@@ -155,7 +135,6 @@ void App::Update()
 	if (!m_bDidPostInit)
 	{
 		m_bDidPostInit = true;
-//		m_special = GetSystemData() != C_PIRATED_NO;
 
 		//build a dummy entity called "GUI" to put our GUI menu entities under
 		Entity *pGUIEnt = GetEntityRoot()->AddEntity(new Entity("GUI"));
@@ -169,57 +148,6 @@ void App::Draw()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	BaseApp::Draw();
-}
-
-void App::OnEnterBackground()
-{
-	BaseApp::OnEnterBackground();
-//	SaveOurStuff();
-
-}
-void App::OnScreenSizeChange()
-{
-	BaseApp::OnScreenSizeChange();
-}
-
-//void App::GetServerInfo( string &server, uint32 &port )
-//{
-//#if defined (_DEBUG) && defined(WIN32)
-//	server = "localhost";
-//	port = 8080;
-
-//	//server = "www.rtsoft.com";
-//	//port = 80;
-//#else
-
-//	server = "rtsoft.com";
-//	port = 80;
-//#endif
-//}
-
-//int App::GetSpecial()
-//{
-//	return m_special; //1 means pirated copy
-//}
-
-//Variant * App::GetVar( const string &keyName )
-//{
-//	return GetShared()->GetVar(keyName);
-//}
-
-std::string App::GetVersionString()
-{
-	return "V0.7";
-}
-
-float App::GetVersion()
-{
-	return 0.7f;
-}
-
-int App::GetBuild()
-{
-	return 1;
 }
 
 const char * GetAppName() {return "SpriteSheets";}
