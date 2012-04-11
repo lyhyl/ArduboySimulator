d4055c4c8bfe69c1997cc420df9e1cd495574503
aki <aki@353e56fe-9613-0410-8469-b96ad8e6f29c>
2012-04-11 17:45:18 +0000

    * Added SpriteAnimationUtils::SetAnimationName * Fixed documentation in VariantTuner

--- shared/util/VariantTuner.h
+++ shared/util/VariantTuner.h
@@ -7,9 +7,14 @@ class Variant;
  * A class that can be used to change a \c Variant's value incrementally with the mouse wheel.
  *
  * This is a development helper class that isn't meant to be included in releases.
- * This class might be helpful for example in fine tuning some physics related constant. Pass some \c Variant that you are trying to find a suitable value for to this class. Then adjust the value incrementally during run time with the mouse wheel and see the effect of different values immediately on the screen. Off course the \c Variant that is manipulated needs to be used somewhere in the code base so that changing its value has a visible effect. This can be generally acchieved in two ways:
+ * This class might be helpful for example in fine tuning some physics related constant.
+ * Pass some \c Variant that you are trying to find a suitable value for to this class.
+ * Then adjust the value incrementally during run time with the mouse wheel and see the
+ * effect of different values immediately on the screen. Off course the \c Variant that
+ * is manipulated needs to be used somewhere in the code base so that changing its value
+ * has a visible effect. This can be generally acchieved in two ways:
  *
- * 1. either pass a \c Variant to this class that is directly used by some other instance. For example:
+ * 1. either pass a \c Variant to this class that is directly used by some other instance:
  *    \code VariantTuner::setTunableVariant(*myEntity->GetVar("pos2d"), CL_Vec2f(1, 0)); \endcode
  * 2. or listen to changes in a \c Variant if it's not directly used in a suitable place:
  * \code
@@ -20,7 +25,7 @@ class Variant;
  *
  * // In some other part of the code
  * static Variant tunable(0.0f);
- * tunable.GetSigOnChanged->connect(boost::bind(&MyListener, _1));
+ * tunable.GetSigOnChanged()->connect(boost::bind(&MyListener, _1));
  * VariantTuner::setTunableVariant(tunable, 0.1f);
  * \endcode
  */
