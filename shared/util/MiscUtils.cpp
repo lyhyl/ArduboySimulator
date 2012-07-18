fbbc5f875904c5fa057a4c8d64347483c4ab5039
aki <aki@353e56fe-9613-0410-8469-b96ad8e6f29c>
2012-07-18 10:02:09 +0000

    * Added RandomAudioPlayerComponent, a component that plays random sounds from a set of files with a random interval. Not used in any of the examples. * StringTokenize returns an empty vector of strings if the input string is an empty string.

--- shared/util/MiscUtils.cpp
+++ shared/util/MiscUtils.cpp
@@ -289,19 +289,22 @@ vector<string> StringTokenize (const  string  & theString,  const  string  & the
 {
 	vector<string> theStringVector;
 
-	size_t  start = 0, end = 0;
-
-	while ( end != string::npos )
+	if (!theString.empty())
 	{
-		end = theString.find( theDelimiter, start );
+		size_t  start = 0, end = 0;
 
-		// If at end, use length=maxLength.  Else use length=end-start.
-		theStringVector.push_back( theString.substr( start,
-			(end == string::npos) ? string::npos : end - start ) );
+		while ( end != string::npos )
+		{
+			end = theString.find( theDelimiter, start );
 
-		// If at end, use start=maxSize.  Else use start=end+delimiter.
-		start = (   ( end > (string::npos - theDelimiter.size()) )
-			?  string::npos  :  end + theDelimiter.size()    );
+			// If at end, use length=maxLength.  Else use length=end-start.
+			theStringVector.push_back( theString.substr( start,
+														 (end == string::npos) ? string::npos : end - start ) );
+
+			// If at end, use start=maxSize.  Else use start=end+delimiter.
+			start = (   ( end > (string::npos - theDelimiter.size()) )
+						?  string::npos  :  end + theDelimiter.size()    );
+		}
 	}
 
 	return theStringVector;
