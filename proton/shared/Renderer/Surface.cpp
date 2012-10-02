34ecbc8442ff959acae79665454f96a5cfbb514e
hamumu <hamumu@353e56fe-9613-0410-8469-b96ad8e6f29c>
2012-10-02 23:48:59 +0000

    * Added BLENDING_DARKEN mode, because MULTIPLY didn't multiply how I wanted

--- proton/shared/Renderer/Surface.cpp
+++ proton/shared/Renderer/Surface.cpp
@@ -585,6 +585,10 @@ void Surface::SetupForRender(const float rotationDegrees, const CL_Vec2f &vRotat
 			glBlendFunc(GL_DST_COLOR, GL_ZERO);
 			glColor4x(GET_RED(rgba) << 8, GET_GREEN(rgba) << 8, GET_BLUE(rgba) << 8, GET_ALPHA(rgba) << 8);
 			break;
+		case BLENDING_DARKEN:
+			glBlendFunc(GL_DST_COLOR, GL_ONE_MINUS_SRC_ALPHA);
+			glColor4x(GET_RED(rgba) << 8, GET_GREEN(rgba) << 8, GET_BLUE(rgba) << 8, GET_ALPHA(rgba) << 8);
+			break;
 		}
 	}
 }
