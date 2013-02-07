53e21172f20e499443e4fc236c3cf6c2bf15984e
seth <seth@353e56fe-9613-0410-8469-b96ad8e6f29c>
2013-02-07 00:43:27 +0000

    * Added GalaxyY size profile * Added IsGalaxySizeY macro * (Android) Added change to allow foreign keyboards enter english easier .. I hope

--- proton/shared/win/app/main.cpp
+++ proton/shared/win/app/main.cpp
@@ -108,7 +108,7 @@ void InitVideoSize()
 	AddVideoMode("Flash", 640, 480, PLATFORM_ID_FLASH);
 
 	//WORK: Change device emulation here
-	string desiredVideoMode = "iPhone4 Landscape"; //name needs to match one of the ones defined above
+	string desiredVideoMode = "iPad Landscape"; //name needs to match one of the ones defined above
  	SetVideoModeByName(desiredVideoMode);
 	GetBaseApp()->OnPreInitVideo(); //gives the app level code a chance to override any of these parms if it wants to
 }
