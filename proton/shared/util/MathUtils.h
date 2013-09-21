f14a472d9f89c25495d33d4d1824cf84e7721171
seth <seth@353e56fe-9613-0410-8469-b96ad8e6f29c>
2013-09-21 07:08:57 +0000

    * Renamed the define PI to RT_PI to fix a name conflict with irrlicht I just noticed, blame Hamumu!

--- proton/shared/util/MathUtils.h
+++ proton/shared/util/MathUtils.h
@@ -38,9 +38,9 @@ CL_Rectf ScaleRect2D(const CL_Rectf &r, CL_Vec2f vScale);
 #define SMOOTHSTEP_INVERSE(x) pow( ((x)/0.5)-1,3)
 
 // converting degrees to radians and vice versa
-#define PI (3.14159265359f)
-#define TO_DEGREES(x) ((x)*180.0f/PI)
-#define TO_RADIANS(x) ((x)*PI/180.0f)
+#define RT_PI (3.14159265359f)
+#define TO_DEGREES(x) ((x)*180.0f/RT_PI)
+#define TO_RADIANS(x) ((x)*RT_PI/180.0f)
 
 bool CircleSegmentIntersect(CL_Vec2f C, float r, CL_Vec2f A, CL_Vec2f B, CL_Vec2f& P);
 float ModNearestInt(float a, float b);
