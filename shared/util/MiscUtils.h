a0582073d17ef12aa62a9b6e113f81047418ed28
seth <seth@353e56fe-9613-0410-8469-b96ad8e6f29c>
2014-05-22 01:17:13 +0000

    Added StringToFloat helper

--- shared/util/MiscUtils.h
+++ shared/util/MiscUtils.h
@@ -87,6 +87,7 @@ std::string toString(C value)
 }
 
 int StringToInt(const std::string &s);
+float StringToFloat(const std::string &s);
 
 std::string PrefixLeading(const std::string input, unsigned int leadingCount, std::string leadingChar, std::string insertAfterPrefix = "");
 std::string PostfixLeading(const std::string input, unsigned int leadingCount, std::string leadingChar, std::string insertAfterPrefix = "");
