a525f38a61edfd343d0ab1946d773cdfe5782e19
hamumu <hamumu@353e56fe-9613-0410-8469-b96ad8e6f29c>
2014-05-23 00:35:26 +0000

    * added ALIGNMENT_RIGHT_CENTER!

--- proton/shared/util/MathUtils.cpp
+++ proton/shared/util/MathUtils.cpp
@@ -118,6 +118,9 @@ CL_Vec2f GetAlignmentOffset(const CL_Vec2f &vSize, eAlignment alignment)
 	case ALIGNMENT_LEFT_CENTER:
 		return CL_Vec2f(0,vSize.y/2);
 		break;
+	case ALIGNMENT_RIGHT_CENTER:
+		return CL_Vec2f(vSize.x,vSize.y/2);
+		break;
 }
 
 	assert(!"Unhandled alignment");
