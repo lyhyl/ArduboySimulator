b4da3f9f6e692189fc1f630b34044334126b58ee
seth <seth@353e56fe-9613-0410-8469-b96ad8e6f29c>
2016-05-28 08:25:58 +0000

    Arduboy simulator base stuff

---
+++ shared/Arduboy/MyEEPROM.h
@@ -0,0 +1,33 @@
+//  ***************************************************************
+//  MyEEPROM - Creation date: 05/26/2016
+//  -------------------------------------------------------------
+//  Robinson Technologies Copyright (C) 2016 - All Rights Reserved
+//
+//  ***************************************************************
+//  Programmer(s):  Seth A. Robinson (seth@rtsoft.com)
+//  ***************************************************************
+
+#ifndef MyEEPROM_h__
+#define MyEEPROM_h__
+
+#include "core/core.h"
+class MyEEPROM
+{
+public:
+	MyEEPROM();
+	virtual ~MyEEPROM();
+
+	/*
+	uint8_t read( int idx )              { return EERef( idx ); }
+	void write( int idx, uint8_t val )   { (EERef( idx )) = val; }
+	void update( int idx, uint8_t val )  { EERef( idx ).update( val ); }
+*/
+	uint8_t read( int idx )              { return 0;}
+	void write( int idx, uint8_t val )   { }
+	void update( int idx, uint8_t val )  { }
+	
+
+private:
+};
+
+#endif // MyEEPROM_h__
