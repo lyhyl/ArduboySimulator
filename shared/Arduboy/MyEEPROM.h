42f8154a77a36465547b577dfb10c1a52ffde5c3
seth <seth@353e56fe-9613-0410-8469-b96ad8e6f29c>
2016-06-09 14:45:03 +0000

    (Arduboy) EEPROM is now fully simulated, it auto saves/loads to a file so it's persistent as well, at least with the windows build.  (html5 build won't be if you refresh the browser..)

--- shared/Arduboy/MyEEPROM.h
+++ shared/Arduboy/MyEEPROM.h
@@ -7,27 +7,34 @@
 //  Programmer(s):  Seth A. Robinson (seth@rtsoft.com)
 //  ***************************************************************
 
+//Emulates the eeprom, saves/loads it to "arduboy_eeprom.dat" in the same dir as the .exe
+
 #ifndef MyEEPROM_h__
 #define MyEEPROM_h__
 
 #include "core/core.h"
+
+#define EEPROM_SIZE 1024 //I guess?
+
+
 class MyEEPROM
 {
 public:
 	MyEEPROM();
 	virtual ~MyEEPROM();
 
-	/*
-	uint8_t read( int idx )              { return EERef( idx ); }
-	void write( int idx, uint8_t val )   { (EERef( idx )) = val; }
-	void update( int idx, uint8_t val )  { EERef( idx ).update( val ); }
-*/
-	uint8_t read( int idx )              { return 0;}
-	void write( int idx, uint8_t val )   { }
-	void update( int idx, uint8_t val )  { }
-	
+	uint8_t read( int idx );
+	void write( int idx, uint8_t val ) ;
+	void update( int idx, uint8_t val ) ;
+
+	//only in proton, for testing
+	void Clear(); 
+
 
 private:
+
+	string m_fileName;
+	uint8_t m_buffer[EEPROM_SIZE];
 };
 
 #endif // MyEEPROM_h__
