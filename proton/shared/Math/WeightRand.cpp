ec7404e57b2409a153e1ff49fc2213c128b52e14
hamumu <hamumu@353e56fe-9613-0410-8469-b96ad8e6f29c>
2016-12-10 04:17:20 +0000

    * added CMiniWeightRand to WeightRand - functions almost the same as CWeightRand, but uses vastly less memory, in exchange for being significantly slower

--- proton/shared/Math/WeightRand.cpp
+++ proton/shared/Math/WeightRand.cpp
@@ -165,3 +165,117 @@ string CWeightRand::TestRun(int runs)
 	}
 	return result;
 }
+
+// ---------------- CMiniWeightRand
+
+CMiniWeightRand::CMiniWeightRand()
+{
+	m_b_needs_computation = true;
+}
+
+int CMiniWeightRand::GetOdds(int i_choice)
+{
+	for(uint32 i=0;i<m_elements.size();i++)
+		if(m_elements[i].num==i_choice)
+			return m_elements[i].odds;
+	return 0;
+}
+
+void CMiniWeightRand::Clear()
+{
+	m_elements.clear();
+	m_b_needs_computation = true;
+}
+
+void CMiniWeightRand::AddChoice(int i_index, int i_odds)
+{
+	m_elements.push_back(MiniRandElement(i_index,i_odds));
+	m_b_needs_computation = true;
+}
+
+void CMiniWeightRand::ComputeOdds()
+{
+	//cycle through and figure out the percentages
+	long l_total = 0;    
+	
+	l_total=GetTotalOdds();
+	for (uint32 i=0; i < m_elements.size(); i++)
+		m_elements[i].computed_odds=float(m_elements[i].odds)/(float)l_total;
+	
+	m_b_needs_computation = false; //all done, ready to compute crap
+}
+
+int CMiniWeightRand::GetItemByPos(int pos)
+{
+	if(pos<0 || pos>=(int)m_elements.size())
+		return 0;
+	return m_elements[pos].num;
+}
+
+long CMiniWeightRand::GetTotalOdds()
+{
+	long l_total = 0;    
+	
+	for (uint32 i=0; i < m_elements.size(); i++)
+	{
+		l_total += m_elements[i].odds;
+	}
+
+	return l_total;
+}
+
+int CMiniWeightRand::CalcNumber(float f_rand)
+{
+	//scroll through and figure out which thing we chose
+	if (m_b_needs_computation) ComputeOdds();
+	float f_temp = 0;
+
+	for (uint32 i=0; i < m_elements.size(); i++)
+	{
+		f_temp += m_elements[i].computed_odds;
+		if (f_temp >= f_rand)
+		{
+			//this must be it.
+			return m_elements[i].num;
+		}
+	}
+
+	return 0;
+}
+
+int CMiniWeightRand::GetRandom(CRandom &rng)
+{
+	if (m_b_needs_computation) ComputeOdds();
+
+	float f_rand = ((float)rng.rand() / (float)RT_RAND_MAX);
+
+	return CalcNumber(f_rand);
+}
+
+int CMiniWeightRand::GetRandom()
+{
+	if (m_b_needs_computation) ComputeOdds();
+
+	float f_rand = (float)rand() / (float)RT_RAND_MAX;
+
+	return CalcNumber(f_rand);
+}
+
+string CMiniWeightRand::TestRun(int runs)
+{
+	int i;
+
+	vector<int> times;
+	times.resize(m_elements.size());
+	for(i=0;i<runs;i++)
+	{
+		int p=GetRandom();
+		times[p]++;
+	}
+	string result="Did "+toString(runs)+" runs, results:\n";
+	for(i=0;i<(int)m_elements.size();i++)
+	{
+		result+="Item #"+toString(m_elements[i].num)+": "+FloatToMoney(((float)times[i]/(float)runs)*100.0f,5)+"% ("+toString(times[i])+" times)\n";
+	}
+	return result;
+}
