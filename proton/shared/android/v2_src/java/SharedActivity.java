e4ad0418d34992b5034f420a9d862ac3555a8fd5
seth <seth@353e56fe-9613-0410-8469-b96ad8e6f29c>
2017-06-18 02:19:31 +0000

    * IAP bugfix with DELAYED orders

--- proton/shared/android/v2_src/java/SharedActivity.java
+++ proton/shared/android/v2_src/java/SharedActivity.java
@@ -696,9 +696,10 @@ m_editText.addTextChangedListener(new TextWatcher()
   
 			   for(Purchase purchase : inventory.getAllPurchases())
 				{
-					//Log.d(PackageName, "Purchase: " + purchase.getSku());
+										//Log.d(PackageName, "Get all purchases Purchase: " + purchase.getSku());
 					//Log.d(PackageName, "json: " + purchase.getOriginalJson());
-					nativeSendGUIStringEx(SharedActivity.app.MESSAGE_TYPE_IAP_PURCHASED_LIST_STATE, 0,0,0, purchase.getSku()); //0 means PURCHASED
+					//Log.d(PackageName, "json: " + purchase.getSignature());
+					nativeSendGUIStringEx(SharedActivity.app.MESSAGE_TYPE_IAP_PURCHASED_LIST_STATE, 0,0,0, purchase.getSku()+"|"+purchase.getOriginalJson()+"|"+purchase.getSignature()); //0 means PURCHASED
 				}
     
             //Log.d(PackageName, "Initial inventory query finished; enabling main UI.");
