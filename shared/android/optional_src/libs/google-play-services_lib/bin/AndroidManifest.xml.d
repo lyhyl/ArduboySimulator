1d9ebf8bf3109f92b81157aaeedc36459a9abba6
seth <seth@353e56fe-9613-0410-8469-b96ad8e6f29c>
2017-07-13 13:12:30 +0000

    - Deleting folders and replacing them can get weird, doing it in its own SVN op

--- shared/android/optional_src/libs/google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\projects\proton\shared\android\optional_src\libs\google-play-services_lib\bin\AndroidManifest.xml : \
-D:\projects\proton\shared\android\optional_src\libs\google-play-services_lib\AndroidManifest.xml \
